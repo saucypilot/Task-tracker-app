890eec7a7b7e937a5a279cdc8374196486919dd5
Irian Durian <durianirian@gmail.com>
2022-11-16 22:37:06 -0600

    Made a button

--- react-task-tracker/src/Components/Header.js
+++ react-task-tracker/src/Components/Header.js
@@ -1,11 +1,28 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+// Header component
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  }
+
   return (
-    <header>
-      <h1>Task Tracker</h1>
+    <header class="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Hello" onClick={onClick}/>
     </header>
   );
 };
 
-Header.defaultProps = {}
+// Default props
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+// Only allow a string to be passed in as a prop and it must be required
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
